310a35f2d372bb57cbde9eb00f76846f18598678
Maria Paz Rojas <mrojas@rayensalud.com>
2018-10-09 12:55:50 -0300

    configuraciones basicas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// RUTAS
+import { APP_ROUTER } from './app.routes';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTER
   ],
   providers: [],
   bootstrap: [AppComponent]
