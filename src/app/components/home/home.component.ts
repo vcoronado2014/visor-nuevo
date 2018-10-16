b66177ddba116b7938a71a73d0c0203393ec76a2
Berenisse Rios <brios@rayensalud.com>
2018-10-16 17:21:19 -0300

    zebra stripes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -200,6 +200,115 @@ export class HomeComponent implements OnInit {
           ]
         }
       ]
+    },
+    {
+      "nombre": "Gripe porcina",
+      "descripcion": "",
+      "estado": "",
+      "tipo": "",
+      "elemento": [
+        {
+          "nombre": "Clasificación Diagnóstica",
+          "descripcion": "",
+          "estado": "",
+          "tipo": "",
+          "elemento": [{
+            "nombre": "No Informado (Confirmada)",
+            "descripcion": "",
+            "estado": "",
+            "tipo": "",
+            "elemento": []
+          }]
+        },
+        {
+          "nombre": "Prescripción",
+          "descripcion": "",
+          "estado": "",
+          "tipo": "",
+          "elemento": [ {
+              "nombre": "Ibuprofeno Compimidos 400 Mg : 1 compimido cada 8 horas...",
+              "descripcion": "",
+              "estado": "",
+              "tipo": "",
+              "elemento": []
+            }
+          ]
+        },
+        {
+          "nombre": "Receta",
+          "descripcion": "",
+          "estado": "",
+          "tipo": "",
+          "elemento": [ {
+              "nombre": "Receta Nro. 204850565, generada 07-11-2017 ",
+              "descripcion": "Tipo Morbilidad, Estado Caducada",
+              "estado": "",
+              "tipo": "",
+              "elemento": [
+                {
+                  "nombre": "Prescripción",
+                  "descripcion": "",
+                  "estado": "",
+                  "tipo": "",
+                  "elemento": [
+                    {
+                      "nombre": "Ibuprofeno Compimidos 400 Mg : 1 compimido cada 8 horas...",
+                      "descripcion": "",
+                      "estado": "",
+                      "tipo": "",
+                      "elemento": []
+                    }]
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "nombre": "Actividad",
+          "descripcion": "",
+          "estado": "",
+          "tipo": "",
+          "elemento": [
+             {
+              "nombre": "Consulta de morbilidad odontológica",
+              "descripción": "",
+              "estado": "",
+              "tipo": "",
+              "elemento": []
+            }
+          ]
+        },
+        {
+          "nombre": "Funcionario Prestador",
+          "descripcion": "",
+          "estado": "",
+          "tipo": "",
+          "elemento": [
+            {
+              "nombre": "RAYEN RAYEN administrador, odontólogo",
+              "descripcion": "",
+              "estado": "",
+              "tipo": "",
+              "elemento": []
+            }
+          ]
+        },
+        {
+          "nombre": "Establecimiento",
+          "descripcion": "",
+          "estado": "",
+          "tipo": "",
+          "elemento": [
+            {
+              "nombre": "RAYENSALUD [CESFAM]",
+              "descripcion": "",
+              "estado": "",
+              "tipo": "",
+              "elemento": []
+            }
+          ]
+        }
+      ]
     }
   ];
 
@@ -233,14 +342,17 @@ export class HomeComponent implements OnInit {
     return this.sistema;
   }	
  
-  evento(e) {
-    console.log(e.srcElement.firstElementChild.id);
-    if(document.getElementById(e.srcElement.firstElementChild.id).classList.contains('fa-plus-square')){
-      document.getElementById(e.srcElement.firstElementChild.id).classList.remove('fa-plus-square'); 
-      document.getElementById(e.srcElement.firstElementChild.id).classList.add('fa-minus-square'); 
-    } else {
-      document.getElementById(e.srcElement.firstElementChild.id).classList.add('fa-plus-square'); 
-      document.getElementById(e.srcElement.firstElementChild.id).classList.remove('fa-minus-square'); 
+  evento(e, p) {
+    /* console.log(e.srcElement.firstElementChild.id); */
+    /* console.log(p.elemento.length); */
+    if(p.elemento.length > 0) {   
+      if(document.getElementById(e.srcElement.firstElementChild.id).classList.contains('fa-plus-square')){
+        document.getElementById(e.srcElement.firstElementChild.id).classList.remove('fa-plus-square'); 
+        document.getElementById(e.srcElement.firstElementChild.id).classList.add('fa-minus-square'); 
+      } else {
+        document.getElementById(e.srcElement.firstElementChild.id).classList.add('fa-plus-square'); 
+        document.getElementById(e.srcElement.firstElementChild.id).classList.remove('fa-minus-square'); 
+      }
     }
   }
 
